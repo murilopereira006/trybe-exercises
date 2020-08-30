29a5edc1344e8255810e03049cb035af866d70c1
murilopereira006 <muriloedu006@gmail.com>
2020-08-30 20:47:29 -0300

    topico 9

---
+++ desenvolvimentoWeb/bloco_4/dia_1/tópico_9/1impar.js
@@ -0,0 +1,11 @@
+var a = 15;
+var b = 10;
+var c = 1;
+
+function par (a, b, c) {
+    if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+        return true;
+    }
+    else {return false;}
+}
+par ();
